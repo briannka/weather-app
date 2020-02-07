ad76d74f587ac97288bd4a849c6f52d551d2027c
Brianna Iliev <brianna.iliev@sumup.com>
2020-02-07 17:23:04 +0200

    server.js initial additions added

--- weather-journal-app/server.js
+++ weather-journal-app/server.js
@@ -2,8 +2,17 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
+const path = require('path');
+const fileName = path.basename('/brianna.iliev/weather-app/weather-journal-app/commentOnlyJS/server.js');
 
 // Start up an instance of app
+const app = express();
+
+// Dependencies
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +20,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
+
+
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8000;
+const server = app.listen(port, () => { console.log(`running on localhost: ${port}`)});
