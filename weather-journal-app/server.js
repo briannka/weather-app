dfdb184b11d1f8ed75a2bf16e5172b5a37c9261b
Brianna Iliev <brianna.iliev@sumup.com>
2020-02-17 17:56:23 +0200

    most recent configurations

--- weather-journal-app/server.js
+++ weather-journal-app/server.js
@@ -1,10 +1,11 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const projectData = [];
 
 // Require Express to run server and routes
 const express = require('express');
 const path = require('path');
-const fileName = path.basename('/brianna.iliev/weather-app/weather-journal-app/commentOnlyJS/server.js');
+const apiKey = '30345c8bb3254064fa5aa95b2598b854';
+const baseUrl = 'api.openweathermap.org/data/2.5/weather?zip=';
 
 // Start up an instance of app
 const app = express();
@@ -14,7 +15,6 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// app.get('/', function(req, res){res.send('hello world')});
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,5 +31,21 @@ app.use(express.static('website'));
 
 
 // Setup Server
+
+app.get('/weather', function(req, res) {
+    let zip = req.query.zip;
+    let feelings = req.query.feelings;
+
+    projectData.push(zip);
+    projectData.push(feelings);
+
+    console.log(projectData);
+    res.send('Is it hot');
+})
+
+
+
+app.get('/', (req, res) => {req.push(projectData), console.log(testing)});
+
 const port = 8000;
 const server = app.listen(port, () => { console.log(`running on localhost: ${port}`)});
--- weather-journal-app/website/app.js
+++ weather-journal-app/website/app.js
@@ -1,5 +1,32 @@
 /* Global Variables */
 
+// Personal API Key for OpenWeatherMap API
+
+// Event listener to add function to existing HTML DOM element
+
+/* Function called by event listener */
+
+/* Function to GET Web API Data*/
+
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+       document.getElementById("entryHolder").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "/weather", true);
+    xhttp.send();
+  }
+loadDoc();
+/* Function to POST data */
+
+
+/* Function to GET Project Data */
+
+
+
+
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
