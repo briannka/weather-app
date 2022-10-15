ec11fc9dce01708da4f45d6d360691fa14420e50
briannka <briannakellyiliev@gmail.com>
2022-10-15 23:52:16 +0300

    hide API key

--- functions/weather.js
+++ functions/weather.js
@@ -1,12 +1,11 @@
 const fetch = require("node-fetch");
 
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 const API_ENDPOINT = "http://api.openweathermap.org/data/2.5/weather";
-const weatherApiKey = "30345c8bb3254064fa5aa95b2598b854";
-
 
 exports.handler = async(event, context) => {
     const { zipCode } = event.queryStringParameters;
-    const weatherURL = `${API_ENDPOINT}?zip=${zipCode},us&appid=${weatherApiKey}`
+    const weatherURL = `${API_ENDPOINT}?zip=${zipCode},us&appid=${WEATHER_API_KEY}`
     const fetchWeatherApi = fetch(weatherURL, { headers: { Accept: "application/json" } });
     return fetchWeatherApi
         .then((response) => response.json())
