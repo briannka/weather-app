17498ceed8cfe403be27eb80e221e1b2fe7b1eb7
briannka <briannakellyiliev@gmail.com>
2022-10-17 18:36:28 +0300

    improve code and toggle results panel

--- website/app.js
+++ website/app.js
@@ -1,10 +1,40 @@
 const weatherEndpoint = "/.netlify/functions/weather";
 
+const generateButton = document.getElementById("generate");
+const resultsPanel = document.getElementById("results_panel");
+const zipInput = document.getElementById("zip");
+const feelingsInput = document.getElementById("feelings");
+
 let d = new Date();
 let newDate = d.getMonth() + 1 + "/" + d.getDate() + "/" + d.getFullYear();
 
+document.addEventListener('DOMContentLoaded', function() {
+    generateButton.style.display = 'block';
+    resultsPanel.style.display = 'none';
+}, false);
 
-const ajax = async(url = "", method = "GET", data) => {
+
+/* Function called by event listener */
+
+generateButton.addEventListener("click", submission);
+
+// Event listener to add function to existing HTML DOM element
+
+async function submission() {
+    const zipValue = zipInput.value;
+    const feelingsValue = feelingsInput.value;
+
+    console.log('zip', zipValue)
+    const dataFromBackend = await fetchBackend(weatherEndpoint, "POST", {
+        zip: zipValue,
+        feelings: feelingsValue
+    });
+    updateUI(dataFromBackend);
+}
+
+// function for calling backend call to weather API
+
+const fetchBackend = async(url = "", method = "GET", data) => {
     const config = {
         method,
         credentials: "same-origin",
@@ -25,7 +55,16 @@ const ajax = async(url = "", method = "GET", data) => {
     }
 };
 
+// function to show results panel on UI
+
+const toggleResultPanelDisplay = () => {
+    generateButton.style.display = 'none';
+    resultsPanel.style.display = 'block';
+}
+
+
 function updateUI(weatherData) {
+    toggleResultPanelDisplay();
     document.getElementById("date").innerText = `Today's date is ${newDate}`;
     document.getElementById(
         "temp"
@@ -33,25 +72,4 @@ function updateUI(weatherData) {
     document.getElementById(
         "content"
     ).innerText = `The user's feeling: ${weatherData.feelings}`;
-}
-
-/* Function called by event listener */
-
-document.getElementById("generate").addEventListener("click", submission);
-
-const zipInput = document.getElementById("zip");
-const feelingsInput = document.getElementById("feelings");
-
-// Event listener to add function to existing HTML DOM element
-
-async function submission() {
-    const zipValue = zipInput.value;
-    const feelingsValue = feelingsInput.value;
-
-    console.log('zip', zipValue)
-    const dataFromBackend = await ajax(weatherEndpoint, "POST", {
-        zip: zipValue,
-        feelings: feelingsValue
-    });
-    updateUI(dataFromBackend);
 }
